ea1b86fad83523512bbd8e6e3493c6dd093c8c57
Alex <corporationasocial@gmail.com>
2023-12-19 08:37:46 +0300

    +Bar

--- src/main/java/ru/asocial/lib2/Library.java
+++ src/main/java/ru/asocial/lib2/Library.java
@@ -2,6 +2,10 @@ package ru.asocial.lib2;
 
 public class Library {
     public void foo() {
-        System.out.println("foo v1.0.0");
+        System.out.println("foo v1.0.1");
+    }
+
+    public void bar() {
+        System.out.println("bar v1.0.1");
     }
 }
